0e94b1858d499354385144b1a768e26236f8bd51
Ruslan Khanbikov <rk760n@att.com>
2019-02-04 12:07:38 -0800

    removing unnecessary stuff

--- k8s/kubeadm/kube-join.sh
+++ k8s/kubeadm/kube-join.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 set -xe
-
+ 
+#$1 - Master IP address
+#$2 - token
+#$3 - hash
+ 
 sudo apt-get update
 sudo apt-get install -y \
     docker.io \
     socat \
     curl \
-    git \
-    ca-certificates \
-    ceph-common \
-    rbd-nbd
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https
-
+    ca-certificates
+ 
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo bash -c 'cat >/etc/apt/sources.list.d/kubernetes.list' << EOF
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
+ 
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
-
+ 
 sudo kubeadm join --token $2 $1:6443 --discovery-token-ca-cert-hash sha256:$3
--- k8s/kubeadm/kube-master.sh
+++ k8s/kubeadm/kube-master.sh
@@ -1,48 +1,40 @@
 #!/bin/bash
 set -xe
-
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com  --recv 460F3994
-RELEASE_NAME=$(grep 'CODENAME' /etc/lsb-release | awk -F= '{print $2}')
-sudo add-apt-repository "deb https://download.ceph.com/debian-mimic/ ${RELEASE_NAME} main"
+ 
+: ${CALICO_VER:="v3.3"}
+ 
+#NOTE: install required packages on host
 sudo apt-get update
 sudo apt-get install -y \
     docker.io \
     socat \
-    git \
     ca-certificates \
-    make \
     nmap \
     curl \
     uuid-runtime \
     jq \
     util-linux \
-    ceph-common \
-    rbd-nbd \
-    nfs-common \
     bridge-utils \
     libxtables11
-sudo tee /etc/modprobe.d/rbd.conf <<EOF
-install rbd /bin/true
-EOF
-
+ 
 sudo apt-get update
-sudo apt-get install -y apt-transport-https
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo bash -c 'cat >/etc/apt/sources.list.d/kubernetes.list' << EOF
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
+ 
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
-
+ 
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16
-
+ 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
-kubectl wait --timeout=240s --for=condition=Ready nodes/rkhanbikov-wrkspc
+ 
+kubectl apply -f https://docs.projectcalico.org/${CALICO_VER}/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/${CALICO_VER}/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+kubectl wait --timeout=240s --for=condition=Ready nodes/$(hostname)
 kubectl --namespace=kube-system wait --timeout=240s --for=condition=Ready pods -l k8s-app=kube-dns
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
